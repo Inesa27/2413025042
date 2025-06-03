4698f66ea2d70f13f920311a858a70bd8546b318
Inesa27 <Inesaaisahwa01@gmail.com>
2025-06-03 14:44:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll("a.disabled-link");
 
-  links.forEach((link) => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      alert("Fitur ini masih dalam pengembangan!");
-    });
-  });
 });
